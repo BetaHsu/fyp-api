8a9944e0a212aaf9f914bf713718c584d295afbf
BetaHsu <ychsu6-c@my.cityu.edu.hk>
2023-04-04 13:17:26 +0200

    2, 4 - check post paragraph

--- controllers/v1/data_controller.py
+++ controllers/v1/data_controller.py
@@ -19,29 +19,23 @@ def hello_world():
     return "<p>API works.</p>"
 
 
-@app.route("/api/v1/add-paragraph")
-def add_paragraph():
-    database_service.add_paragraph()
-    return "<p>Added paragraph.</p>"
-
-
 @app.route("/api/v1/get-paragraph", methods=["GET", "OPTIONS"])
 def get_paragraph():
     paragraph = {
         'title_interval_start': 439,
         'title_interval_end': 476,
-        "paragraph": "Through decades that ran like rivers, endless rivers of endless woes. Through pick and shovel sjambok and jail. O such a long long journey! When the motor-car came, the sledge and the ox-cart began to die. But for a while the bicycle made in Britain, was the dream of every village boy. With the arrival of the bus, the city was brought into the village, and we began to yearn for the place behind the horizons. Such a long travail it was. A long journey from bush to concrete. ",
+        "paragraph": "Through decades that ran like rivers, endless rivers of endless woes. Through pick and shovel sjambok and jail. O such a long long journey!When the motor-car came, the sledge and the ox-cart began to die. But for a while the bicycle made in Britain, was the dream of every village boy. With the arrival of the bus, the city was brought into the village, and we began to yearn for the place behind the horizons. Such a long travail it was. A long journey from bush to concrete. ",
         "id": "flG47F77IQ",
         "creator_id": "vjakukfe",
         "revealed": [
             {
                 "index_interval_start": 0,
-                "index_interval_end": 20,
+                "index_interval_end": 439,
                 "revealed_score": 1,
             },
             {
-                "index_interval_start": 20,
-                "index_interval_end": 476,
+                "index_interval_start": 439,
+                "index_interval_end": 477,
                 "revealed_score": 0,
             }
         ]
